072e2a1cd2e5b31b50a2e58dcea7e57489fa6752
NicholaChen <nicholachen08@gmail.com>
2025-09-21 15:41:25 -0500

    deleted camera.java

--- TeamCode/camera.java
+++
@@ -1,9 +0,0 @@
-public class RobotCamera {
-
-    public void init(HardwareMap) {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        OpenCvWebcam camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam1"), cameraMonitorViewId);
-        FtcDashboard.getInstance().startCameraStream(camera, 0);
-
-    }
-}
