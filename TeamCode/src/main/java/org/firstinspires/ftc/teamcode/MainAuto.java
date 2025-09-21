a96df81d100ed35347e72ef793b943997fb886c6
NicholaChen <nicholachen08@gmail.com>
2025-09-21 12:36:22 -0500

    added MainAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Config
+@Autonomous
+public class MainAuto extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpMode.java
@@ -1,23 +1,35 @@
 package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Config
 @TeleOp(name="Main OpMode", group="Main")
 public class MainTeleOpMode extends OpMode {
+    private FtcDashboard dash = FtcDashboard.getInstance();
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
+    private DcMotor intake = null;
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
+        telemetry = new MultipleTelemetry(telemetry, dash.getTelemetry());
         telemetry.addData("Status", "Initialized");
 
         // Initialize the hardware variables. Note that the strings used here as parameters
@@ -27,6 +39,8 @@ public class MainTeleOpMode extends OpMode {
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
@@ -61,12 +75,35 @@ public class MainTeleOpMode extends OpMode {
      */
     @Override
     public void loop() {
+
+        TelemetryPacket packet = new TelemetryPacket();
+
+        // updated based on gamepads
+
+//        // update running actions
+//        List<Action> newActions = new ArrayList<>();
+//        for (Action action : runningActions) {
+//            action.preview(packet.fieldOverlay());
+//            if (action.run(packet)) {
+//                newActions.add(action);
+//            }
+//        }
+//        runningActions = newActions;
+
+        dash.sendTelemetryPacket(packet);
+
         double max;
 
         // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
         double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
         double lateral =  gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
+
+        if (gamepad1.dpad_left) {
+            intake.setPower(1.0);
+        } else {
+            intake.setPower(0.0);
+        }
 
         // Combine the joystick requests for each axis-motion to determine each wheel's power.
         // Set up a variable for each drive wheel to save the power level for telemetry.
